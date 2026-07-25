9a7ae89c894526d0af55eec6453e2dcd8f6d4c39
seaikh91-Asif <seaikh91@gmail.com>
2026-07-25 14:53:02 +0530

    rename the project

---
+++ 04-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='h-screen w-full bg-gray-600' ></div>
+    </>
+  )
+}
+
+export default App
