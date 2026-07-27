ca8a5eb7d1ce447ea7ef50a49d393856ae0b2ad6
seaikh91-Asif <seaikh91@gmail.com>
2026-07-27 10:55:02 +0530

    left hero content finished

---
+++ 05-User_Section_Project/src/Componenets/Section_001/HeroText.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div  className='p-6'>
+            <h3 className='text-7xl font-bold mb-7 leading-[1.1] ' >
+                Prospective <br /> <span>customer</span> <br /> sugmentatin
+            </h3>
+            <p className='text-xl font-medium text-gray-600' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi nulla totam praesentium at perferendis omnis. Unde ad qui doloribus?
+            </p>
+        </div>
+  )
+}
+
+export default HeroText
--- 05-User_Section_Project/src/Componenets/Section_001/Page_01_Contant.jsx
+++ 05-User_Section_Project/src/Componenets/Section_001/Page_01_Contant.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import LeftContent from './LeftContent'
+import RightContent from './RightContent'
 
 const Page_01_Contant = () => {
   return (
-    <div>Page_01_Contant</div>
+    <div className=' flex gap-10 items-center py-3 h-[90vh] px-18 '>
+        <LeftContent/>
+        <RightContent/>
+
+    </div>
   )
 }
 
