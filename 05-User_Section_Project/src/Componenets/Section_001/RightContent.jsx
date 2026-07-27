4e82a24a03da14a12193ca2fcfd00cbc1416bf6b
seaikh91-Asif <seaikh91@gmail.com>
2026-07-27 15:30:49 +0530

    mc error

--- 05-User_Section_Project/src/Componenets/Section_001/RightContent.jsx
+++ 05-User_Section_Project/src/Componenets/Section_001/RightContent.jsx
@@ -5,9 +5,9 @@ const RightContent = (props) => {
   return (
     <div className='h-full flex flex-nowrap gap-10 p-6 w-7/10  '>
         
-    {props.users?.map(function(elem){
+    {props.users?.map(function(elem, idx){
       
-      return <RightCard  img = {elem.img} />
+      return <RightCard key = {idx} img = {elem.img} tag = {elem.tag} intro = {elem.intro} />
     })}
     </div>
   )
--- 05-User_Section_Project/src/Componenets/Section_001/Section_01.jsx
+++ 05-User_Section_Project/src/Componenets/Section_001/Section_01.jsx
@@ -4,6 +4,7 @@ import Page_01_Contant from './Page_01_Contant'
 
 const Section_01 = (props) => {
   return (
+   
     <div>
       <Navbar/>
       <Page_01_Contant users = {props.users} />
