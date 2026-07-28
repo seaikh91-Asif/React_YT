1f3c3c6bd854662fec8a4e328ad0ac632bf46216
seaikh91-Asif <seaikh91@gmail.com>
2026-07-28 15:01:47 +0530

    counter project cmplete

--- 07_Hooks_React/src/App.jsx
+++ 07_Hooks_React/src/App.jsx
@@ -10,19 +10,32 @@ const App = () => {
     setnum( num + 1); 
   }
 
+  const changeNumm = () => {
+    setnum(num - 1); 
+  }
+
+  const changeNummm = () => {
+    setnum(0); 
+  }
+
   return (
     <>
-      <div className='parent'>
+     <div className='parent'>
         <div className='container'>
 
-            <h1>this is counter: {num} </h1>
+            <h1>This is counter: {num} </h1>
             <h2>This is Counter:{num} </h2>
-            <h3>this is counter: {num} </h3>
-            <h4>this is counter: {num}</h4>
-            <h5>this is counter: {num} </h5>
-            <h6>this is counter: {num} </h6>
+            <h3>This is counter: {num} </h3>
+            <h4>This is counter: {num}</h4>
+            <h5>This is counter: {num} </h5>
+            <h6>This is counter: {num} </h6>
 
-            <button onClick={changeNum} >Change Counter </button>
+            <div className="buttons">
+              <button onClick={changeNumm}>Decrease Counter</button>
+            
+            <button onClick={changeNummm}>Reset Value</button>
+            <button onClick={changeNum} >Increase Counter </button>
+            </div>
         </div>
     </div>
     </>
