00362aa62137c66f803042f71ab7be1c9eed5cb3
seaikh91-Asif <seaikh91@gmail.com>
2026-08-11 16:06:50 +0530

    single page application done

---
+++ 15_React_Router_DOM/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <h2>Routing</h2>
+        <div>
+          <Link  to='/'> Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </div>
+      </div>
+  )
+}
+
+export default Navbar
